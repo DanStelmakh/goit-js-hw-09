9b490195fd6758f5f1be758a161a7d6c63043b2f
DanStelmakh <danylo.stelmakh@gmail.com>
2022-09-09 10:47:45 +0300

    change color by clicking btn

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', changeColor);
+btnStop.addEventListener('click', stopChangeColor);
+
+function changeColor() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function stopChangeColor() {
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+
+  clearInterval(timerId);
+}
